845d62614d21b23f875043ddf8eb6e10836b7693
Hamdani Ilham <d41113512@gmail.com>
2025-07-26 14:45:57 +0800

    fix: timestamps

--- database/migrations/2022_11_10_204808_create_pengajuan_ijins_table.php
+++ database/migrations/2022_11_10_204808_create_pengajuan_ijins_table.php
@@ -18,8 +18,8 @@ return new class extends Migration
             $table->string('kode_perusahaan');
             $table->string('nip');
             $table->string('kode_ijin');
-            $table->timestamp('tanggal_mulai');
-            $table->timestamp('tanggal_selesai');
+            $table->timestamp('tanggal_mulai')->nullable();
+            $table->timestamp('tanggal_selesai')->nullable();
             $table->string('keterangan')->nullable();
             $table->string('file')->nullable();
             $table->string('status')->default(0);
